a552f0ba28f764e67b2505b138ba5a7143d883af
andregomezradix <andre.filho@radixeng.com>
2024-05-03 08:56:10 -0300

    Update MESFactoryTalk API

---
+++ OPCComm/OPCServer.cs
@@ -0,0 +1,98 @@
+﻿using MESOPCServerMinimalAPI.DTO;
+using Newtonsoft.Json;
+using Opc.Ua;
+using Opc.Ua.Client;
+
+namespace MESOPCServerMinimalAPI.OPCServer
+{
+public class OPCServer
+{
+    private readonly IConfiguration? _config;
+    private readonly CustomLog _customLog;
+
+    public OPCServer(IConfiguration? config, CustomLog customLog)
+    {
+        _config = config;
+        _customLog = customLog;
+    }
+
+    public async Task<string?> GetAllTagsAsync()
+    {
+        string? serverUrl = _config?.GetSection("OPCServerConfig:ServerUrl").Value;
+        List<string>? tags = _config.GetSection("OPCServerConfig:Tags").Value.Split(",").ToList();
+
+        try
+        {
+            var config = new ApplicationConfiguration
+
+            {
+                ApplicationName = "OPC UA Client",
+                ApplicationType = ApplicationType.Client,
+                SecurityConfiguration = new SecurityConfiguration
+                {
+                    ApplicationCertificate = new CertificateIdentifier(),
+                    TrustedPeerCertificates = new CertificateTrustList(),
+                    TrustedIssuerCertificates = new CertificateTrustList(),
+                    RejectedCertificateStore = new CertificateTrustList(),
+                    AutoAcceptUntrustedCertificates = true
+                },
+                TransportQuotas = new TransportQuotas { OperationTimeout = 60000 },
+                ClientConfiguration = new ClientConfiguration { DefaultSessionTimeout = 60000 },
+                TraceConfiguration = new TraceConfiguration(),
+
+            };
+
+            var endpointDescription = CoreClientUtils.SelectEndpoint(serverUrl, false);
+            var endpointConfiguration = EndpointConfiguration.Create(config);
+            var endpoint = new ConfiguredEndpoint(null, endpointDescription, endpointConfiguration);
+            var session = await Session.Create(config, endpoint, false, "", 60000, null, null);
+
+
+            if (session != null && session.Connected)
+            {
+                _customLog.Log($"Information: Conectado ao servidor OPC UA: {serverUrl}");
+
+                var tagDataList = new List<TagData>();
+
+                foreach (string tag in tags)
+                {
+                    try
+                    {
+                        ExpandedNodeId nodeId = ExpandedNodeId.Parse(tag);
+                        var readResult = await session.ReadValueAsync((NodeId)nodeId);
+
+                        tagDataList.Add(new TagData
+                        {
+                            Tag = tag,
+                            Value = readResult.Value,
+                            Timestamp = readResult.SourceTimestamp,
+                            Quality = readResult.StatusCode.ToString()
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        _customLog.Log($"Error: Erro ao ler a tag {tag}: {ex.Message}");
+                    }
+                }
+
+                string json = JsonConvert.SerializeObject(tagDataList, Newtonsoft.Json.Formatting.Indented);
+
+                await session.CloseAsync();
+                _customLog.Log("Information: Desconectado do servidor OPC UA.");
+
+                return json;
+            }
+            else
+            {
+                _customLog.Log($"Information: Falha ao conectar ao servidor OPC UA: {serverUrl}");
+                return null;
+            }
+        }
+        catch (Exception ex)
+        {
+            _customLog.Log($"Error: Erro ao conectar ao servidor OPC UA: {ex.Message}");
+            return null;
+        }
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+using MESOPCServerMinimalAPI.OPCServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var config = builder.Configuration;
+
+string logPath = config["LogPath"];
+var customLog = new CustomLog(logPath);
+
+try
+{
+    builder.Services.AddSingleton(customLog);
+
+    var app = builder.Build();
+
+    app.MapGet("/opcservertags", async () =>
+    {
+        var OCPObj = new OPCServer(config, customLog);
+        var OPCTags = await OCPObj.GetAllTagsAsync();
+
+        return OPCTags;
+    });
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    customLog.Log("Warning: Um erro inesperado ocorreu: {0}", ex.Message);
+}
